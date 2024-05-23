5ea97c853e6c0d32e00e3cb48ed5e2c1f52ad99e
leobuck <leobuck12@gmail.com>
2024-05-22 21:13:48 -0300

    Tipos das variáveis

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Olá Mundo")
+	var nome string = "Leo"
+	var idade int = 26
+	var versao float32 = 1.1
+	fmt.Println("Olá,", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
 }
