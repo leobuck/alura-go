0c825534ffad974c4590271a8bc4489342bff16f
leobuck <leobuck12@gmail.com>
2024-05-22 21:22:39 -0300

    Inferência de tipos

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,11 +1,13 @@
 package main
 
 import "fmt"
+import "reflect"
 
 func main() {
-	var nome string = "Leo"
-	var idade int = 26
-	var versao float32 = 1.1
+	nome := "Leo"
+	idade := 26
+	versao := 1.1
 	fmt.Println("Olá,", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão", versao)
+	fmt.Println("O tipo da variável versao é", reflect.TypeOf(versao))
 }
