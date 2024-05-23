9f711683ec86a7dc27c284956661a23e319050a6
leobuck <leobuck12@gmail.com>
2024-05-22 21:34:11 -0300

    Exibindo o menu

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,13 +1,19 @@
 package main
 
 import "fmt"
-import "reflect"
 
 func main() {
 	nome := "Leo"
-	idade := 26
 	versao := 1.1
-	fmt.Println("Olá,", nome, "sua idade é", idade)
+	fmt.Println("Olá,", nome)
 	fmt.Println("Este programa está na versão", versao)
-	fmt.Println("O tipo da variável versao é", reflect.TypeOf(versao))
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("O comando escolhido foi", comando)
 }
